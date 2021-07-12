10220c82892a95977155a2eb0df28708efd2e95d
rkaeh <rkaehd127@gmail.com>
2021-07-12 17:17:22 +0900

    7/12 2ban class commit_1

--- accountapp/views.py
+++ accountapp/views.py
@@ -1,8 +1,10 @@
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render
 
 
 # Create your views here.
+from django.urls import reverse
+
 from accountapp.models import HelloWorld
 
 
@@ -15,9 +17,10 @@ def hello_world(request):
         new_data.text = temp
         new_data.save()
 
-        return render(request, 'accountapp/hello_world.html',
-                      context={'new_data': new_data})
+        return HttpResponseRedirect(reverse('accountapp:hello_world'))
+
     else:
+        data_list = HelloWorld.objects.all()
         return render(request, 'accountapp/hello_world.html',
-                      context={'text': 'GET METHOD!'})
+                      context={'data_list': data_list})
 
