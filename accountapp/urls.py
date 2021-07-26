9dd24b56d10c7e16ec3ba10486a9bec6830c396f
rkaeh <rkaehd127@gmail.com>
2021-07-26 15:44:40 +0900

    7/12 2ban class commit_3

--- accountapp/urls.py
+++ accountapp/urls.py
@@ -1,10 +1,12 @@
 from django.urls import path
 
-from accountapp.views import hello_world
+from accountapp.views import hello_world, AccountCreateView
 
 app_name = 'accountapp'
 
 urlpatterns = [
-    path('hello_world2/', hello_world, name='hello_world')
+    path('hello_world2/', hello_world, name='hello_world'),
+
+    path('create/', AccountCreateView.as_view(), name='create')
 ]
 
--- accountapp/views.py
+++ accountapp/views.py
@@ -1,9 +1,12 @@
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
 from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render
 
 
 # Create your views here.
-from django.urls import reverse
+from django.urls import reverse, reverse_lazy
+from django.views.generic import CreateView
 
 from accountapp.models import HelloWorld
 
@@ -26,3 +29,8 @@ def hello_world(request):
         return render(request, 'accountapp/hello_world.html',
                       context={'data_list': data_list})
 
+class AccountCreateView(CreateView):
+    model = User
+    form_class = UserCreationForm
+    success_url = reverse_lazy('accoutapp:hello_world') # 클래스에서 경로를 다시만들어주는 함수를 reverse_lazy()
+    template_name = 'accountapp/create.html'  # 어느 html file을 이용할지
