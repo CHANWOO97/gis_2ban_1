c176c2e2d5c9c92fec6d18e2f3437487e65aa90e
rkaeh <rkaehd127@gmail.com>
2021-07-26 14:54:30 +0900

    7/12 2ban class commit_2

--- accountapp/views.py
+++ accountapp/views.py
@@ -17,10 +17,12 @@ def hello_world(request):
         new_data.text = temp
         new_data.save()
 
-        return HttpResponseRedirect(reverse('accountapp:hello_world'))
+
+
+        return HttpResponseRedirect(reverse('accountapp:hello_world')) # 해당하는 경로를 다시 만들어주는 함수 reverse()
 
     else:
-        data_list = HelloWorld.objects.all()
+        data_list = HelloWorld.objects.all()  # DB에서 object 객체를 all 모두 긁어온다 이런식으로 데이터베이스 긁어온다
         return render(request, 'accountapp/hello_world.html',
                       context={'data_list': data_list})
 
